53aa683a1d5ec0a4c42520e3436db681230653ee
lautaroilla <lautaro12387@gmail.com>
2023-01-21 06:25:36 +0000

    Task 0

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""task 0"""
+
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    """API"""
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users/')
+    user = user.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    todos = todos.json()
+    dicty = {}
+    _list = []
+    for i in user:
+        _list = [{"task": j.get('title'),
+                 "completed": j.get('completed'),
+                  "username": i.get('username')} for j in todos
+                 if j.get('userId') == i.get('id')]
+        dicty["{}".format(i.get('id'))] = _list
+    with open("todo_all_employees.json", 'w', encoding="UTF-8") as file:
+        w = json.dump(dicty, file)
